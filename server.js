08cc8d936917d59a728b6c0b91f231207a95b74c
Tria Thao <triathao@gmail.com>
2021-01-09 16:31:24 -0800

    server modification

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const exphbs = require("express-handlebars");
 // Create an instance of the express app.
 const app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
-
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
